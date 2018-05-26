451c1a8c5616aa6fb47a018e83381d1de0eb1b8d
annashcherbina <annashcherbina@gmail.com>
2018-05-26 18:03:41 -0400

    aggregate_atac.sh

--- age/aggregate_atac.sh
+++ age/aggregate_atac.sh
@@ -1,4 +1,28 @@
 #get a merged peak file
+rm naive_overlap.optimal_set.bed
+rm naive_overlap.optimal_set.sorted.bed
+rm naive_overlap.optimal_set.sorted.merged.bed
 
-#get read counts for each rep for the merged peak file 
-bedtools coverage -counts -a merged_peaks/ppr.merged.bed -b SG2M_R2_treated/out/align/pooled_pseudo_reps/ppr2/slopped | cut -f4 > SG2M_R2_treated_pr2
+for peak_file in `cut -f2 atac.peaks.naiveo.age.txt`
+do
+    zcat $peak_file >> naive_overlap.optimal_set.bed 
+done
+bedtools sort -i naive_overlap.optimal_set.bed > naive_overlap.optimal_set.sorted.bed
+bedtools merge -i naive_overlap.optimal_set.sorted.bed > naive_overlap.optimal_set.sorted.merged.bed
+
+echo "generated merged peak file!"
+
+#get the counts for each sample within each peak 
+export numfiles=`cat atac.tagalign.age.txt| wc -l`
+echo $numfiles
+for i in $(seq 1 $numfiles)
+do
+    cur_sample_name=`head -n $i atac.tagalign.age.txt | tail -n1 | cut -f1`
+    echo $cur_sample_name > counts.$cur_sample_name.txt
+    cur_tagalign=`head -n $i atac.tagalign.age.txt | tail -n1 | cut -f2`
+    echo $cur_sample_name
+    bedtools coverage -counts -a naive_overlap.optimal_set.sorted.merged.bed -b $cur_tagalign | cut -f4 >> counts.$cur_sample_name.txt  
+done
+paste counts.*.txt > age.atac.counts.txt
+#clean up temporary files 
+rm counts.*.txt
