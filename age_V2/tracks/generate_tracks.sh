9d97f293c464fc37f419d9083051acb159d80b6a
anna shcherbina <annashcherbina@gmail.com>
2019-06-26 15:43:52 -0700

    auto-make tracks

---
+++ age_V2/tracks/generate_tracks.sh
@@ -0,0 +1,13 @@
+python generate_tracks.py --pval_bigwigs /mnt/lab_data2/annashch/nobel_lab_projects/age_V2/tracks/ATAC_seq/pval.tracks.txt \
+    --fc_bigwigs /mnt/lab_data2/annashch/nobel_lab_projects/age_V2/tracks/ATAC_seq/fc.tracks.txt \
+    --hammocks /mnt/lab_data2/annashch/nobel_lab_projects/age_V2/tracks/ATAC_seq/hammock.tracks.txt \
+    --rna_bigwigs /mnt/lab_data2/annashch/nobel_lab_projects/age_V2/tracks/RNA_seq/rna.tracks.txt \
+    --outf_fc_bigwig age_fc_bigwig.json \
+    --outf_pval_bigwig age_pval_bigwig.json \
+    --mitra_prefix /srv/www/kundaje/annashch/age/ \
+    --prefix_to_drop_for_oak /mnt/lab_data2/annashch/nobel_lab_projects/age_V2/
+
+
+
+
+
