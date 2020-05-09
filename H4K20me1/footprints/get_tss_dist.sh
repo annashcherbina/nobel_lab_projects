8e51e92a7561313046079bf6d5c11a10f7568062
annashcherbina <annashcherbina@gmail.com>
2020-05-09 03:08:56 -0400

    footprint map

---
+++ H4K20me1/footprints/get_tss_dist.sh
@@ -0,0 +1,3 @@
+bedtools closest -a /nas/homes/annashch/H4K20me1/croo/croo_Old_H4K20me1/peak/overlap_reproducibility/overlap.optimal_peak.broadPeak.gz -b /data/mm10/ataqc/mm10_gencode_tss_unique.bed.gz -d > Old.distance.bed
+bedtools closest -a /nas/homes/annashch/H4K20me1/croo/croo_Young_H4K20me1/peak/overlap_reproducibility/overlap.optimal_peak.broadPeak.gz -b /data/mm10/ataqc/mm10_gencode_tss_unique.bed.gz -d > Young.distance.bed
+
---
+++ H4K20me1/footprints/make_heatmap.sh
@@ -0,0 +1,10 @@
+computeMatrix reference-point --samplesLabel "Aged" "Young" \
+	      -p 50 \
+	      -S 5p.counts.Old.bw  5p.counts.Young.bw \
+	      -R Merged.tss.near.peaks.bed \
+	      -o h4k20me1.mat.gz \
+	      --upstream 5000 \
+	      --downstream 5000 \
+	      --binSize 40 
+
+plotHeatmap --colorMap RdBu --zMin 0 --zMax 0.075 --kmeans 4 -m h4k20me1.mat.gz -out h4k20me1.svg 
