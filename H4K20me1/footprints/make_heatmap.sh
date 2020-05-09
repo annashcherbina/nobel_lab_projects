ad0ac8befab0a1f1a024ab4d4cebf9ce0dfd5c6a
annashcherbina <annashcherbina@gmail.com>
2020-05-09 03:11:58 -0400

    footprint

--- H4K20me1/footprints/make_heatmap.sh
+++ H4K20me1/footprints/make_heatmap.sh
@@ -4,7 +4,6 @@ computeMatrix reference-point --samplesLabel "Aged" "Young" \
 	      -R Merged.tss.near.peaks.bed \
 	      -o h4k20me1.mat.gz \
 	      --upstream 5000 \
-	      --downstream 5000 \
-	      --binSize 40 
+	      --downstream 5000
 
-plotHeatmap --colorMap RdBu --zMin 0 --zMax 0.075 --kmeans 4 -m h4k20me1.mat.gz -out h4k20me1.svg 
+plotHeatmap --colorMap RdBu --zMin 0 --zMax 0.075 -m h4k20me1.mat.gz -out h4k20me1.svg 
