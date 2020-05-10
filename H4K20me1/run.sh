befeb269c4722092ce6d1040d3b6154d5db557b5
annashcherbina <annashcherbina@gmail.com>
2020-05-10 14:23:21 -0400

    cleanup

--- H4K20me1/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-caper submit /opt/chip-seq-pipeline2/chip.wdl -i pipeline.Old.json
-caper submit /opt/chip-seq-pipeline2/chip.wdl -i pipeline.Young.json
-
--- H4K20me1/run_croo.sh
+++
@@ -1,3 +0,0 @@
-croo --out-dir croo_Young_H4K20me1 --method copy /nas/homes/annashch/H4K20me1/caper_out/chip/72cab657-4520-4eee-b95a-d87eac1cecbf/metadata.json 
-croo --out-dir croo_Old_H4K20me1 --method copy /nas/homes/annashch/H4K20me1/caper_out/chip/7aab8e70-c16d-481f-ab6d-ebd0e3683fae/metadata.json 
-
